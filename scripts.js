24890412fd995d7633576e9a468e4040ed5f4fbf
Tom Wood <tomwood1875@gmail.com>
2025-05-07 09:25:58 +0100

    Refactor: Rename container to gridContainer for clarity

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
-const container = document.createElement("div");
-container.classList.add("container");
-document.body.appendChild(container);
+const gridContainer = document.createElement("div");
+gridContainer.classList.add("grid-container");
+document.body.appendChild(gridContainer);
 
 function createGrid() {
     for (let i = 1; i <= 256; i++) {
         let newDiv = document.createElement("div");
         newDiv.classList.add("grid-square");
-        container.appendChild(newDiv);
+        gridContainer.appendChild(newDiv);
     
         newDiv.addEventListener("mouseover", function() {
             newDiv.classList.add("on-mouse-over");

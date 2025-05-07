097b6182b44733b89b713073fd16cd80f89c9a8e
Tom Wood <tomwood1875@gmail.com>
2025-05-07 09:03:31 +0100

    Add functionality to change background color of grid-square on mouseover

--- scripts.js
+++ scripts.js
@@ -2,10 +2,16 @@ const container = document.createElement("div");
 container.classList.add("container");
 document.body.appendChild(container);
 
-for (let i = 1; i <= 256; i++) {
-   let newDiv = document.createElement("div");
-   newDiv.classList.add("grid-square");
-   container.appendChild(newDiv);
-
+function createGrid() {
+    for (let i = 1; i <= 256; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("grid-square");
+        container.appendChild(newDiv);
+    
+        newDiv.addEventListener("mouseover", function() {
+            newDiv.classList.add("on-mouse-over");
+    });
+    }
 }
 
+createGrid();
